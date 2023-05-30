8c545219ecb5eb481fc0972e1a46050a77f4e6bd
Filip Slanina <10fili10@seznam.cz>
2023-05-30 13:34:27 +0200

    Added auto docstring

--- HackerRank/test.py
+++ HackerRank/test.py
@@ -1,7 +1,21 @@
 import math
+
 ab = 10
 bc = 10
-def calculateAngle(ab, bc):
+
+
+def calculateAngle(ab: int, bc: int):
+    """calculateAngle _summary_
+
+    Args:
+        ab (int): _description_
+        bc (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
     ac = pow(pow(ab, 2) + pow(bc, 2), 0.5)
-    return round(math.atan(ab/ac)*(180/math.pi))
-result = print(calculateAngle(ab,bc))
+    return round(math.atan(ab / ac) * (180 / math.pi))
+
+
+result = print(calculateAngle(ab, bc))
